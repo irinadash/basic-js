bb6d00072860b3c87bdf389e8a01b122d02702b2
IrinaDash <irina.lev@list.ru>
2021-03-12 22:27:36 +0300

    init: basic-js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let str = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  members.forEach((element) => {
+    if (typeof element === 'string') {
+      const res = element.split(/\s/).join('');
+      str.push(res[0].toUpperCase());
+    }
+  });
+
+  str.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  str = str.join('');
+  return str;
+}
